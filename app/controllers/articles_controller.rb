7c4254b3acd79e9f9692794ffa155fa8b203f45b
pchellovod <pchellovod@gmail.com>
2016-07-25 17:20:16 +0500

    add create method

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+  def new
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+  @article = Article.new(params.require(:article).permit(:title, :text))
+
+  @article.save
+  redirect_to @article
+  end
+end
+
+
+# private
+#   def article_params
+#     params.require(: [ article = Article.new (article_params)] article).permit(:title, :text)
+#   end
+# end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'welcome/index' #говорит Rails направлять запросы к http://localhost:3000/welcome/index в экшн index контроллера welcome
+
+
+
+  resources :articles
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index' #говорит Rails направить запросы к корню приложения в экшн index контроллера welcome
+end
